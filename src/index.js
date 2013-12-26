328a7151a480402a4c0c9008baa5e27499886536
futurez <xshapex16@googlemail.com>
2013-12-26 23:42:12 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -39,62 +39,73 @@ mywallet.prototype.apiCall = function(path, callback) {
 };
 
 mywallet.prototype.payment = function(params, callback) {
-	if(!(params.to))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("to")))
 		throw "to missing";
-	if(!(params.amount))
+	if(!(params.hasOwnProperty("amount")))
 		throw "amount missing";
 
 	var path = "/de/merchant/" + this.guid + "/payment?password=" + this.password + "&to=" + params.to + "&amount=" + params.amount;
-	if(params.second_password)
+	if(params.hasOwnProperty("second_password"))
 		path += "&second_password=" + params.second_password;
-	if(params.from)
+	if(params.hasOwnProperty("from"))
 		path += "&from=" + params.from;
-	if(params.shared)
+	if(params.hasOwnProperty("shared"))
 		path += "&shared=" + params.shared;
-	if(params.fee)
+	if(params.hasOwnProperty("fee"))
 		path += "&fee=" + params.fee;
-	if(params.note)
+	if(params.hasOwnProperty("note"))
 		path += "&note=" + params.note;
 
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.multiplePayments = function(params, callback) {
-	if(!(params.recipients))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("recipients")))
 		throw "recipients missing";
 
 	var path = "/de/merchant/" + this.guid + "/sendmany?password=" + this.password + "&recipients=" + params.recipients;
-	if(params.second_password)
+	if(params.hasOwnProperty("second_password"))
 		path += "&second_password=" + params.second_password;
-	if(params.from)
+	if(params.hasOwnProperty("from"))
 		path += "&from=" + params.from;
-	if(params.shared)
+	if(params.hasOwnProperty("shared"))
 		path += "&shared=" + params.shared;
-	if(params.fee)
+	if(params.hasOwnProperty("fee"))
 		path += "&fee=" + params.fee;
-	if(params.note)
+	if(params.hasOwnProperty("note"))
 		path += "&note=" + params.note;
 
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.getBalance = function(callback) {
-	var path = "/de/merchant/$guid/balance?password=" + this.password;
+	var path = "/de/merchant/" + this.guid + "/balance?password=" + this.password;
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.getAddresses = function(params, callback) {
 	var path = "/de/merchant/" + this.guid + "/list?password=" + this.password;
-	if(params.confirmations)
-		path += "?confirmations=" + params.confirmations;
+	if(params) {
+		if(params.hasOwnProperty("confirmations"))
+			path += "?confirmations=" + params.confirmations;
+	}
 
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.getAddressBalance = function(params, callback) {
-	if(!(params.address))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("address")))
 		throw "address missing";
-	if(!(params.confirmations))
+	if(!(params.hasOwnProperty("confirmations")))
 		throw "confirmations missing";
 
 	var path = "/de/merchant/" + this.guid + "/address_balance?password=" + this.password + "&address=" + params.address + "&confirmations=" + params.confirmations;
@@ -103,43 +114,60 @@ mywallet.prototype.getAddressBalance = function(params, callback) {
 
 mywallet.prototype.generateAddress = function(params, callback) {
 	var path = "/de/merchant/" + this.guid + "/new_address?password=" + this.password;
-	if(params.second_password)
-		path += "&second_password=" + params.second_password;
-	if(params.label)
-		path += "&label=" + params.label;
+	if(params) {
+		if(params.hasOwnProperty("second_password"))
+			path += "&second_password=" + params.second_password;
+		if(params.hasOwnProperty("label"))
+			path += "&label=" + params.label;
+	}
 
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.archiveAddress = function(params, callback) {
-	if(!(params.address))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("address")))
 		throw "address missing";
 
 	var path = "/de/merchant/" + this.guid + "/archive_address?password=" + this.password + "&address=" + params.address;
-	if(params.second_password)
-		path += "&second_password=" + params.second_password;
+	if(params) {
+		if(params.hasOwnProperty("second_password"))
+			path += "&second_password=" + params.second_password;
+	}
 
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.unarchiveAddress = function(params, callback) {
-	if(!(params.address))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("address")))
 		throw "address missing";
 
 	var path = "/de/merchant/" + this.guid + "/unarchive_address?password=" + this.password + "&address=" + params.address;
-	if(params.second_password)
-		path += "&second_password=" + params.second_password;
+	if(params) {
+		if(params.hasOwnProperty("second_password"))
+			path += "&second_password=" + params.second_password;
+	}
 	
 	this.apiCall(path, callback);
 };
 
 mywallet.prototype.consolidate = function(params, callback) {
-	if(!(params.days))
+	if(!(params))
+		throw "params missing";
+
+	if(!(params.hasOwnProperty("days")))
 		throw "days missing";
 
 	var path = "/de/merchant/" + this.guid + "/auto_consolidate?password=" + this.password + "&days=" + params.days;
-	if(params.second_password)
-		path += "&second_password=" + params.second_password;
+	if(params) {
+		if(params.hasOwnProperty("second_password"))
+			path += "&second_password=" + params.second_password;
+	}
 
 	this.apiCall(path, callback);
 };
